2e76acb67cedbc335a90f2ac0bdcd7b39c53adf0
EASTinphectiON <107661289+EASTinphectiON@users.noreply.github.com>
2022-11-10 20:24:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EGG-Finder
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qn7kio)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Egg Finder</h1>`;
+
